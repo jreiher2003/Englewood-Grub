fdf47d05cf9c751edc288f1a52e04a3453007a76
Jeff Reiher <jeffreiher@gmail.com>
2015-09-24 18:06:50 -0400

    add Address class called addresses which is a many-to-one table linked to the User class by users.id=addresses.user_id

--- SQLAlchemy-lesson1/sqlalchemy_orm_tutorial.py
+++ SQLAlchemy-lesson1/sqlalchemy_orm_tutorial.py
@@ -1,7 +1,9 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Sequence 
+from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
+from sqlalchemy.orm import relationship, backref
+
 Base = declarative_base()
 
 class User(Base):
@@ -14,7 +16,17 @@ class User(Base):
      def __repr__(self):
          return "<User(name='%s', fullname='%s', password='%s')>" % (self.name, self.fullname, self.password)
 
+class Address(Base):
+	__tablename__ = 'addresses'
+	id = Column(Integer, primary_key=True)
+	email_address = Column(String, nullable=False)
+	user_id = Column(Integer, ForeignKey('users.id'))
+	user = relationship("User", backref=backref('addresses', order_by=id))
+
+	def __repr__(self):
+		return "<Address(emil_address='%s')>" % self.email_address
+
 
 engine = create_engine('sqlite:///orm.db', echo=True)
-
+# create table statement in SQL
 Base.metadata.create_all(engine)
