e3ae91ac965921b94bc2015784a2011722506894
Jeff Reiher <jeffreiher@gmail.com>
2015-09-24 17:52:17 -0400

    turned queries into functions

--- SQLAlchemy-lesson1/sql_orm_session.py
+++ SQLAlchemy-lesson1/sql_orm_session.py
@@ -30,33 +30,39 @@ jeff = User(name="jeff", fullname="Jeff Reiher", password="12345")
 
 ##########################################################
 # Querying
-# jeff.name = 'jeff'
-# session.rollback()
-# session.add(jeff)
-# session.commit()
-name = session.query(User).order_by(User.id)
-for i in name:
-	print i.name, i.fullname
+def user_query():
+	name = session.query(User).order_by(User.id)
+	for i in name:
+		print i.name, i.fullname
 
-print "\n"
 
-for name, fullname in session.query(User.name, User.fullname):
-	print fullname, name
+def user_query2():
+	for name, fullname in session.query(User.name, User.fullname):
+		print fullname, name
 
-print "\n"
 
-for name in session.query(User.name).filter(User.fullname=='Jeff Reiher'):
-	print name
+def user_filter():
+	for name in session.query(User.name).filter(User.fullname=='Jeff Reiher'):
+		print name
 
-print "\n"
 
-query = session.query(User).filter(User.name.like('%jeff')).order_by(User.id)
-print query.all()
+def user_like():
+	query = session.query(User).filter(User.name.like('%jeff')).order_by(User.id)
+	print query.all()
+	print query.first()
 
-print "\n"
-print query.first()
-# print query.one()
-print "\n"
 
-new_query = session.query(User).filter(User.name.like("%jeff")).count()
-print new_query
+def user_count():
+	new_query = session.query(User).filter(User.name.like("%jeff")).count()
+	print new_query
+
+if __name__ == '__main__':
+	user_query()
+	print "\n"
+	user_query2()
+	print "\n"
+	user_filter()
+	print "\n"
+	user_like()
+	print "\n"
+	user_count()
