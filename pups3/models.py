832d528ab20669f8c32c06044ed9420d27961914
Jeff Reiher <jeffreiher@gmail.com>
2015-11-10 17:09:25 -0500

    add pups3 starter code with begining models.py and sqlite3 cli and populater file to have entries in db

---
+++ pups3/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Shelter(Base):
+	__tablename__ = 'shelter'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(80), nullable=False)
+	address = Column(String(250))
+	city = Column(String(40))
+	state = Column(String(20))
+	zipCode = Column(String(10))
+	website = Column(String)
+
+engine = create_engine("sqlite:///puppyshelter.db")
+
+Base.metadata.create_all(engine)
