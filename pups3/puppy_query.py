a7edde301bf9c4e12a0ce5ecfd1076016540708f
Jeff Reiher <jeffreiher@gmail.com>
2015-11-11 14:35:40 -0500

    add new puppy query functions

--- pups3/puppy_query.py
+++ pups3/puppy_query.py
@@ -5,7 +5,30 @@ from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
 
+def query_all():
+	all_puppies = session.query(Puppy).order_by(Puppy.name)
+	for puppy in all_puppies:
+		print puppy.name
 
+def puppy_age():
+	p_age = session.query(Puppy).\
+		filter(Puppy.dateOfBirth.between('2015-05-11', '2015-11-11')).\
+		order_by(Puppy.dateOfBirth.desc())
+	for age in p_age:
+		print age.name, age.dateOfBirth
 
+def puppy_weight():
+	weight = session.query(Puppy).filter(Puppy.weight).order_by(Puppy.weight.asc())
+	for w in weight:
+		print w.name, w.weight
 
-if __main__ == '__name__':
+def shelters_where_puppies_are():
+	shel = session.query(Shelter,Puppy).filter(Shelter.id==Puppy.shelter_id).order_by(Shelter.name)
+	for s, p in shel:
+		print s.name, p.name
+
+if __name__ == '__main__':
+	# query_all()
+	# puppy_age()
+	# puppy_weight()
+	shelters_where_puppies_are()
