b8ac9b4d570a689520742886a3995866627a8142
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-19 18:53:12 -0500

    Update db with Adpoters and assoicaition class.

--- problem set 2/puppy-adoption/app/models.py
+++ problem set 2/puppy-adoption/app/models.py
@@ -3,6 +3,11 @@ from app import db
 from sqlalchemy.orm import relationship 
 
 # Base = declarative_base()
+association_table = db.Table('association', db.Base.metadata,
+    db.Column('puppy_id', db.Integer, db.ForeignKey('puppy.id')),
+    db.Column('adopter_id', db.Integer, db.ForeignKey('adopters.id'))
+    )
+
 
 class Shelter(db.Model):
 
@@ -19,6 +24,7 @@ class Shelter(db.Model):
     def __repr__(self):
         return '<name>: {}'.format(self.name)
 
+
 class Puppy(db.Model):
 
     __tablename__ = "puppy"
@@ -31,9 +37,39 @@ class Puppy(db.Model):
     shelter_id = db.Column(db.Integer, db.ForeignKey('shelter.id'))
     shelter = relationship(Shelter)
     weight = db.Column(db.Numeric(10))
+    profile = relationship("Profile", uselist=False, back_populates="puppy")
+    adopters = relationship("Adpoters", secondary=association_table,back_populates="puppy")
 
     def __repr__(self):
         return '<name>: {}'.format(self.name)
 
 
-# 
+class Profile(db.Model):
+
+    __tablename__ = "profile"
+
+    id = db.Column(db.Integer, primary_key=True)
+    photo = db.Column(db.String)
+    description = db.Column(db.String(500))
+    specialNeeds = db.Column(db.String(500))
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppy.id'))
+    puppy = relationship("Puppy", back_populates="profile")
+
+    def __repr__(self):
+        return '<name>: {}'.format(self.name)
+        
+
+class Adpoters(db.Model):
+
+    __tablename__ = "adopters"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    puppies = relationship('Puppy', secondary=association_table,back_populates='adopters')
+
+    def __repr__(self):
+        return '<name>: {}'.format(self.name)
+
+
+
+
