1c0ee20be4a285df8d768cdb3edbd71db4cf025b
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-22 11:05:48 -0500

    Configs to keep db out of vc

--- problem set 2/puppy-adoption/app/__init__.py
+++ problem set 2/puppy-adoption/app/__init__.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask 
 from flask.ext.sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__) 
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
 from app import views
---
+++ problem set 2/puppy-adoption/env.bat
@@ -0,0 +1,2 @@
+set APP_SETTINGS=config.DevelopmentConfig
+set DATABASE_URL = postgresql://jeff:finn7797@localhost:5432/pups
