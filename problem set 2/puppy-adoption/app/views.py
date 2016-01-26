5b86e041f6ecfc4e7f86f2083bc50e0646ae97c5
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-26 10:34:31 -0500

    Create puppy profile page with dynamic urls

--- problem set 2/puppy-adoption/app/views.py
+++ problem set 2/puppy-adoption/app/views.py
@@ -18,7 +18,13 @@ def shelter_profile(shelter_id,shelter_name):
 	shelter_profile = db.session.query(Shelter).filter_by(id=shelter_id).one()
 	puppy = db.session.query(Puppy).filter_by(shelter_id=shelter_id).all()
 	return render_template('shelter_profile.html', shelter_profile=shelter_profile, puppy=puppy)
-	
+
+
+@app.route('/<int:shelter_id>/<path:shelter_name>/<int:puppy_id>')
+def puppy_profile(shelter_id,shelter_name,puppy_id):
+	puppy = db.session.query(Puppy).filter_by(id=puppy_id).one()
+	return render_template('puppy_profile.html', puppy=puppy)
+
 ## CRUD operations Puppy, Shelter Adoptors ##
 #############################################
 
