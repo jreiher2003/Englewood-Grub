8f2622df7bb7537d72939d7bbc5de3694ea8ce06
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-28 14:04:46 -0500

    Update forms.py add description field

--- problem set 2/puppy-adoption/app/forms.py
+++ problem set 2/puppy-adoption/app/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import Form 
-from wtforms import TextField, RadioField
-from wtforms.validators import DataRequired, url
+from wtforms import TextField, RadioField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class CreatePuppy(Form):
 	name = TextField('Name', validators=[DataRequired()])
 	gender = RadioField('Gender', choices=[('female', 'Female'), ('male', 'Male')])
 	picture = TextField('Photo-Url')
+	specialNeeds = RadioField('Special Needs', choices=[('None','None'),('3-legged', '3-legged'), ('Blind', 'Blind'),('Deaf', 'Deaf')])
+	description = TextAreaField('Description', validators=[Length(max=500)])
