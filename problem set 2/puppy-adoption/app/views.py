fa883398c384e9ed31e0b5ebd3b05750db6542e8
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-28 14:07:40 -0500

    Update edit puppy function and delete url

--- problem set 2/puppy-adoption/app/views.py
+++ problem set 2/puppy-adoption/app/views.py
@@ -46,9 +46,13 @@ def new_puppy():
 	form = CreatePuppy()
 	if form.validate_on_submit():
 		newpuppy = Puppy(name=form.name.data, gender=form.gender.data, dateOfBirth=create_random_age(), picture=form.picture.data, shelter_id=add_puppy_to_shelter(), weight=create_random_weight())
+		db.session.add(newpuppy)
+		db.session.commit()
+		newprofile = Profile(specialNeeds=form.specialNeeds.data,description=descriptions(), puppy_id=newpuppy.id)
+		db.session.add(newprofile)
+		db.session.commit()
 		currentcapacity = db.session.query(Shelter).filter(Shelter.id==newpuppy.shelter_id).one()
 		currentcapacity.current_capacity = currentcapacity.current_capacity + 1
-		db.session.add(newpuppy)
 		db.session.add(currentcapacity)
 		db.session.commit()
 		flash('Successfully Added a Puppy to '+ currentcapacity.name, 'success')
@@ -56,10 +60,27 @@ def new_puppy():
 
 	return render_template('new_puppy.html', form=form)
 
-@app.route('/<int:shelter_id>/<path:shelter_name>/profile/<int:puppy_id>/edit/')
+
+@app.route('/<int:shelter_id>/<path:shelter_name>/profile/<int:puppy_id>/edit/', methods=['GET','POST'])
 def edit_puppy(shelter_id,shelter_name,puppy_id):
-	puppy = db.session.query(Puppy).filter_by(id=puppy_id).one()
-	return render_template("edit_puppy_profile.html", puppy=puppy)
+	# editpuppy = db.session.query(Puppy).filter_by(id=puppy_id).one()
+	# edit_pro = db.session.query(Profile).filter_by(puppy_id=puppy_id).one()
+	editpuppy=db.session.query(Puppy).join(Profile, Puppy.id==Profile.puppy_id).filter(Puppy.id==puppy_id).one()
+	form = CreatePuppy(obj=editpuppy)
+	if form.validate_on_submit():
+		editpuppy.name = form.name.data
+		editpuppy.gender = form.gender.data
+		editpuppy.picture = form.picture.data
+		editpuppy.profile.specialNeeds = form.specialNeeds.data
+		db.session.add(editpuppy)
+		db.session.commit()
+		flash('Puppy was successfully edited', 'success')
+		return redirect(url_for('index'))
+	return render_template("edit_puppy_profile.html", editpuppy=editpuppy, form=form)
+
+@app.route('/<int:shelter_id>/<path:shelter_name>/profile/<int:puppy_id>/delete/', methods=['GET','POST'])
+def delete_puppy(shelter_id, shelter_name, puppy_id):
+	return 'delete'
 # create a new shelter ######################
 # create a new adoptor ######################
 
