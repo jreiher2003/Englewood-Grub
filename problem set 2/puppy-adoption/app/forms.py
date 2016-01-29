0b8ad4df18e24e40709b6626b53bbf311ee957e0
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-28 21:37:56 -0500

    Add imput types to forms

--- problem set 2/puppy-adoption/app/forms.py
+++ problem set 2/puppy-adoption/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form 
-from wtforms import TextField, RadioField, BooleanField, TextAreaField, SelectField
+from wtforms import TextField, RadioField, BooleanField, TextAreaField, SelectField, IntegerField
 from wtforms.validators import DataRequired, Length, URL, NumberRange
 
 
@@ -7,6 +7,6 @@ class CreatePuppy(Form):
 	name = TextField('Name', validators=[DataRequired()])
 	gender = RadioField('Gender', choices=[('female', 'Female'), ('male', 'Male')])
 	picture = TextField('Photo-Url')
-	weight = TextField('Weight')
+	weight = IntegerField('Weight')
 	specialNeeds = SelectField('Special Needs', choices=[('None','None'),('3-legged', '3-legged'), ('Blind', 'Blind'),('Deaf', 'Deaf')])
 	description = TextAreaField('Description', validators=[Length(max=500)])
