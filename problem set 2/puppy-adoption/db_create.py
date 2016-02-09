b3fe5d1f884c97e349e38956e8c71220b6f99dda
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-09 13:22:42 -0500

    Recreate db

--- problem set 2/puppy-adoption/db_create.py
+++ problem set 2/puppy-adoption/db_create.py
@@ -8,6 +8,13 @@ import random
 
 db.create_all() 
 
+def count_pups():
+	shelter = db.session.query(Shelter).all()
+	for shel in shelter:
+		shel.current_capacity = db.session.query(Puppy, Shelter).join(Shelter).filter(Shelter.id == shel.id).count()
+		db.session.add(shel)
+		db.session.commit()
+
 adoptor1 = Adoptors(name="Jeff")
 db.session.add(adoptor1)
 adopter2 = Adoptors(name="Mark")
@@ -19,19 +26,19 @@ db.session.add(adopter4)
 db.session.commit()
 
 # Add Shelters
-shelter1 = Shelter(name = "Oakland Animal Services", address = "1101 29th Ave", city = "Oakland", state = "California", zipCode = "94601", website = "oaklandanimalservices.org", maximum_capacity=randint(25,28))
+shelter1 = Shelter(name = "Oakland Animal Services", address = "1101 29th Ave", city = "Oakland", state = "California", zipCode = "94601", website = "oaklandanimalservices.org", maximum_capacity=randint(25,28), current_capacity=count_pups())
 db.session.add(shelter1)
 
-shelter2 = Shelter(name = "San Francisco SPCA Mission Adoption Center", address="250 Florida St", city="San Francisco", state="California", zipCode = "94103", website = "sfspca.org", maximum_capacity=randint(25,28))
+shelter2 = Shelter(name = "San Francisco SPCA Mission Adoption Center", address="250 Florida St", city="San Francisco", state="California", zipCode = "94103", website = "sfspca.org", maximum_capacity=randint(25,28), current_capacity=count_pups())
 db.session.add(shelter2)
 
-shelter3 = Shelter(name = "Wonder Dog Rescue", address= "2926 16th Street", city = "San Francisco", state = "California" , zipCode = "94103", website = "http://wonderdogrescue.org", maximum_capacity=randint(25,28))
+shelter3 = Shelter(name = "Wonder Dog Rescue", address= "2926 16th Street", city = "San Francisco", state = "California" , zipCode = "94103", website = "http://wonderdogrescue.org", maximum_capacity=randint(25,28), current_capacity=count_pups())
 db.session.add(shelter3)
 
-shelter4 = Shelter(name = "Humane Society of Alameda", address = "PO Box 1571" ,city = "Alameda" ,state = "California", zipCode = "94501", website = "hsalameda.org", maximum_capacity=randint(25,28))
+shelter4 = Shelter(name = "Humane Society of Alameda", address = "PO Box 1571" ,city = "Alameda" ,state = "California", zipCode = "94501", website = "hsalameda.org", maximum_capacity=randint(25,28), current_capacity=count_pups())
 db.session.add(shelter4)
 
-shelter5 = Shelter(name = "Palo Alto Humane Society" ,address = "1149 Chestnut St." ,city = "Menlo Park", state = "California" ,zipCode = "94025", website = "paloaltohumane.org", maximum_capacity=randint(25,28))
+shelter5 = Shelter(name = "Palo Alto Humane Society" ,address = "1149 Chestnut St." ,city = "Menlo Park", state = "California" ,zipCode = "94025", website = "paloaltohumane.org", maximum_capacity=randint(25,28), current_capacity=count_pups())
 db.session.add(shelter5)
 
 db.session.commit()
@@ -42,7 +49,7 @@ male_names = ["Bailey", "Max", "Charlie", "Buddy","Rocky","Jake", "Jack", "Toby"
 
 female_names = ['Bella', 'Lucy', 'Molly', 'Daisy', 'Maggie', 'Sophie', 'Sadie', 'Chloe', 'Bailey', 'Lola', 'Zoe', 'Abby', 'Ginger', 'Roxy', 'Gracie', 'Coco', 'Sasha', 'Lily', 'Angel', 'Princess','Emma', 'Annie', 'Rosie', 'Ruby', 'Lady', 'Missy', 'Lilly', 'Mia', 'Katie', 'Zoey', 'Madison', 'Stella', 'Penny', 'Belle', 'Casey', 'Samantha', 'Holly', 'Lexi', 'Lulu', 'Brandy', 'Jasmine', 'Shelby', 'Sandy', 'Roxie', 'Pepper', 'Heidi', 'Luna', 'Dixie', 'Honey', 'Dakota']
 
-puppy_images = ["https://pixabay.com/static/uploads/photo/2015/11/17/13/13/bulldog-1047518_960_720.jpg", "https://pixabay.com/static/uploads/photo/2015/03/26/09/54/pug-690566__180.jpg","https://pixabay.com/static/uploads/photo/2014/03/05/19/23/dog-280332__180.jpg","https://pixabay.com/static/uploads/photo/2015/02/05/12/09/chihuahua-624924__180.jpg","https://pixabay.com/static/uploads/photo/2016/01/05/17/57/dog-1123026__180.jpg","https://pixabay.com/static/uploads/photo/2014/03/14/20/07/painting-287403__180.jpg","https://pixabay.com/static/uploads/photo/2016/01/05/17/51/dog-1123016__180.jpg","https://pixabay.com/static/uploads/photo/2014/07/05/08/50/puppy-384647__180.jpg","https://pixabay.com/static/uploads/photo/2015/12/23/14/29/puppies-1105730__180.jpg","https://pixabay.com/static/uploads/photo/2015/11/17/12/42/puppy-1047454__180.jpg"]
+puppy_images = ["https://pixabay.com/static/uploads/photo/2015/03/26/09/54/pug-690566__180.jpg","https://pixabay.com/static/uploads/photo/2014/03/05/19/23/dog-280332__180.jpg","https://pixabay.com/static/uploads/photo/2015/02/05/12/09/chihuahua-624924__180.jpg","https://pixabay.com/static/uploads/photo/2016/01/05/17/57/dog-1123026__180.jpg","https://pixabay.com/static/uploads/photo/2014/03/14/20/07/painting-287403__180.jpg","https://pixabay.com/static/uploads/photo/2016/01/05/17/51/dog-1123016__180.jpg","https://pixabay.com/static/uploads/photo/2014/07/05/08/50/puppy-384647__180.jpg","https://pixabay.com/static/uploads/photo/2015/12/23/14/29/puppies-1105730__180.jpg","https://pixabay.com/static/uploads/photo/2015/11/17/12/42/puppy-1047454__180.jpg"]
 
 
 
@@ -142,13 +149,7 @@ populate_profile()
 
 
 ### update shelter to add the current capacity
-def count_pups():
-	shelter = db.session.query(Shelter).all()
-	for shel in shelter:
-		shel.current_capacity = db.session.query(Puppy, Shelter).join(Shelter).filter(Shelter.id == shel.id).count()
-		db.session.add(shel)
-		db.session.commit()
-count_pups()
+# count_pups()
 
 # def add_puppy_to_shelter():
 # 	arr = [1,2,3,4,5]
@@ -180,11 +181,11 @@ def counting_shows():
 counting_shows()
 
 # make sure maximum_capacity > current_capacity
-def overflow(shelter_id):
-	shelter = Shelter.query.filter(Shelter.shelter_id).one()
+# def overflow(shelter_id):
+# 	shelter = Shelter.query.filter(Shelter.shelter_id).one()
 	
-	if shelter.current_capacity < shelter.maximum_capacity:
-		return True
-	else:
-		return False
+# 	if shelter.current_capacity < shelter.maximum_capacity:
+# 		return True
+# 	else:
+# 		return False
 # print overflow()
