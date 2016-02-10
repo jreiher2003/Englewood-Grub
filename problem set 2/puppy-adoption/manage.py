d14f275847c1ca4058c7e57683f00ed5a0e32621
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-09 20:16:35 -0500

    Add coverage and test function to manage.py

--- problem set 2/puppy-adoption/manage.py
+++ problem set 2/puppy-adoption/manage.py
@@ -4,11 +4,17 @@ from app import app,db
 from flask.ext.script import Manager 
 from flask.ext.migrate import Migrate, MigrateCommand 
 
-# app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.add_command
+def test():
+	"""Runs the tests without coverage."""
+	tests = unittest.Testloader().discover('.')
+	unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
 	manager.run()
