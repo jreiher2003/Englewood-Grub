4190fe09b6df2890c48e614331fb94ea8e7ff951
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-23 14:21:09 -0500

    Add travis ci and coverage to manage.py

--- Iterative Development - lesson4/menu/app/views.py
+++ Iterative Development - lesson4/menu/app/views.py
@@ -1,6 +1,6 @@
-from app import app,db
-from flask import render_template
+from app import app,db # pragma: no cover
+from flask import render_template # pragma: no cover
 
-@app.route('/')
+@app.route('/') # pragma: no cover
 def index():
 	return render_template('index.html')
