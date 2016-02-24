a9874afe6f1fd8723df2657ec801028045b0d62c
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-23 20:56:48 -0500

    Add test for place table

--- Iterative Development - lesson4/menu/tests/test_front_page.py
+++ Iterative Development - lesson4/menu/tests/test_front_page.py
@@ -1,14 +1,24 @@
 import unittest 
 from base import BaseTestCase 
+from app import app,db 
+from app.models import Place
 
 class TestFrontPage(BaseTestCase):
 
     def test_index_loads(self):
         response = self.client.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Hello World', response.data)
+        self.assertIn(b'Testname', response.data)
 
     def test_stylesheet_loads(self):
         response = self.client.get('/static/styles.css', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+
+    def test_place_database(self):
+    	place = Place.query.filter_by(id=1).one()
+    	self.assertEqual(place.id, 1)
+    	self.assertEqual(place.name, 'Testname')
+
+
+
 
